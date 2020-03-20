8cd9d4261d6fc0eb6e5f732bbd422e48d56ef25c
AndreyAmelchenia <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-03-20 14:47:25 +0300

    docs: index Ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( array && array != 0 && (array.length != 0))
+  {if(Array.isArray(array)) {
+    let min = array[0];
+    array.forEach(el => {
+      if (min > el){
+        min = el
+      }
+    })
+    return min;
+  } else {
+    return 0;
+  }} else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( array && array != 0 && (array.length != 0))
+  {if(Array.isArray(array)) {
+    let max = array[0];
+    array.forEach(el => {
+      if (max < el){
+        max = el
+      }
+    })
+    return max;
+  } else {
+    return 0;
+  }} else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( array && array != 0 && (array.length != 0))
+  {if(Array.isArray(array)) {
+    let avg = 0;
+    array.forEach(el => {
+      avg +=el;
+    })
+    avg = avg/(array.length)
+    return avg;
+  } else {
+    return 0;
+  }} else {
+    return 0;
+  }
 }
